6462177c0b2527c97ebe0346d3cf7b12ceb0af2b
Andres Pereira <110078992+0xQuake@users.noreply.github.com>
2024-10-23 13:10:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
-// main.cpp
 #include "ListasCirculares.h"
 
 int main() {
-    // Crear personas
+    
     Persona p1("Erwe", 25);
     Persona p2("Von", 20);
     Persona p3("Esse", 30);
     Persona p4("Maria", 22);
     
-    // Probar lista circular simplemente enlazada con personas
     CircularSinglyLinkedList<Persona> listaSimple;
     
-    // Insertar datos desordenados
     listaSimple.insertEnd(p1);
     listaSimple.insertEnd(p2);
     listaSimple.insertEnd(p3);
@@ -19,7 +16,6 @@ int main() {
     cout << "Lista circular simplemente enlazada desordenada:" << endl;
     listaSimple.printList();
     
-    // Insertar datos ordenados por edad
     CircularSinglyLinkedList<Persona> listaSimpleOrdenada;
     listaSimpleOrdenada.insertSorted(p1);
     listaSimpleOrdenada.insertSorted(p2);
@@ -29,10 +25,8 @@ int main() {
     cout << "Lista circular simplemente enlazada ordenada por edad:" << endl;
     listaSimpleOrdenada.printList();
     
-    // Probar lista circular doblemente enlazada con personas
     CircularDoublyLinkedList<Persona> listaDoble;
     
-    // Insertar datos desordenados
     listaDoble.insertEnd(p1);
     listaDoble.insertEnd(p2);
     listaDoble.insertEnd(p3);
@@ -40,7 +34,6 @@ int main() {
     cout << "Lista circular doblemente enlazada desordenada:" << endl;
     listaDoble.printList();
     
-    // Insertar datos ordenados por edad
     CircularDoublyLinkedList<Persona> listaDobleOrdenada;
     listaDobleOrdenada.insertSorted(p1);
     listaDobleOrdenada.insertSorted(p2);
